9ea9102b26199df1481d9dc67af4550e4bfaa07d
Peter Mallett <peter.mallett@mediacurrent.com>
2019-05-31 15:55:10 -0600

    192. Complex State with useState

--- src/index.js
+++ src/index.js
@@ -1,47 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-// // Bad example - this component does not work correctly.
-// const App = (props) => {
-//   // const [count, setCount] = useState(props.initCount);
-//   // const [text, setText] = useState('');
-//   const [state, setState] = useState({
-//     count: props.initCount,
-//     text: '',
-//   });
+const NoteApp = () => {
+  const notesData = JSON.parse(localStorage.getItem('notes'));
+  const [notes, setNotes] = useState(notesData || []);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
 
-//   return (
-//     <div>
-//       <p>The current {state.text || 'count'} is {state.count}</p>
-//       <button onClick={() => setState({ count: state.count - 1 })}>-1</button>
-//       <button onClick={() => setState({ count: props.initCount })}>Reset</button>
-//       <button onClick={() => setState({ count: state.count + 1 })}>+1</button>
-//       <input value={state.text} onChange={(e) => setState({ text: e.target.value })} />
-//     </div>
-//   );
-// };
+  const addNote = (e) => {
+    e.preventDefault();
+    setNotes([
+      ...notes,
+      { title, body },
+    ]);
+    setTitle('');
+    setBody('');
+  };
 
-const App = (props) => {
-  const [count, setCount] = useState(props.initCount);
-  const [text, setText] = useState('');
+  const removeNote = (title) => {
+    setNotes(notes.filter((note) => note.title !== title));
+  };
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  });
 
   return (
     <div>
-      <p>The current {text || 'count'} is {count}</p>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-      <button onClick={() => setCount(props.initCount)}>Reset</button>
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <h1>Notes</h1>
+      {notes.map((note) => (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+          <button onClick={() => removeNote(note.title)}>x</button>
+        </div>
+      ))}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <textarea value={body} onChange={(e) => setBody(e.target.value)} />
+        <button>add note</button>
+      </form>
     </div>
   );
 };
 
-App.defaultProps = {
-  initCount: 0,
-};
+// const App = (props) => {
+//   const [count, setCount] = useState(props.initCount);
+//   const [text, setText] = useState('');
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+//   useEffect(() => {
+//     console.log('useEffect ran');
+//     document.title = count;
+//   });
+
+//   return (
+//     <div>
+//       <p>The current {text || 'count'} is {count}</p>
+//       <button onClick={() => setCount(count - 1)}>-1</button>
+//       <button onClick={() => setCount(props.initCount)}>Reset</button>
+//       <button onClick={() => setCount(count + 1)}>+1</button>
+//       <input value={text} onChange={(e) => setText(e.target.value)} />
+//     </div>
+//   );
+// };
+
+ReactDOM.render(<NoteApp />, document.getElementById('root'));
+// ReactDOM.render(<App initCount={0} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
