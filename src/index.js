2d9a416d183af7956e59f01a0511e730a5a9a304
Peter Mallett <peter.mallett@mediacurrent.com>
2019-06-07 17:19:14 -0600

    195. Cleaning up Effects

--- src/index.js
+++ src/index.js
@@ -36,11 +36,7 @@ const NoteApp = () => {
     <div>
       <h1>Notes</h1>
       {notes.map((note) => (
-        <div key={note.title}>
-          <h3>{note.title}</h3>
-          <p>{note.body}</p>
-          <button onClick={() => removeNote(note.title)}>x</button>
-        </div>
+        <Note key={note.title} note={note} removeNote={removeNote} />
       ))}
       <p>Add note</p>
       <form onSubmit={addNote}>
@@ -48,6 +44,24 @@ const NoteApp = () => {
         <textarea value={body} onChange={(e) => setBody(e.target.value)} />
         <button>add note</button>
       </form>
+    </div>
+  );
+};
+
+const Note = ({ note, removeNote}) => {
+  useEffect(() => {
+    console.log('Setting up effect.');
+
+    return () => {
+      console.log('Cleaning up effect.');
+    };
+  }, []);
+
+  return (
+    <div>
+      <h3>{note.title}</h3>
+      <p>{note.body}</p>
+      <button onClick={() => removeNote(note.title)}>x</button>
     </div>
   );
 };
